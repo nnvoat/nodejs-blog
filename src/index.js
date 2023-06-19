01e58b27a6176b63d97965051ca6192385a3027d
nnvoat <nnvoat@gmail.com>
2023-06-19 14:39:52 +0700

    create routes and app/controllers

--- src/index.js
+++ src/index.js
@@ -5,25 +5,23 @@ const exphbs = require("express-handlebars");
 const app = express();
 const port = 3000;
 
+const route = require("./routes/index.js");
+
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 app.engine("hbs", exphbs.engine({ extname: ".hbs" }));
 app.set("view engine", "hbs");
 app.set("views", path.join(__dirname, "resources/views"));
-console.log(__dirname);
-console.log(path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
-
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
